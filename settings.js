486483f708b69110d99c7c3f7d411de53739065b
Rock de Vocht <rock@peter.conz>
2019-08-04 12:17:24 +1200

    added better comments to settings.js

--- settings.js
+++ settings.js
@@ -1,12 +1,13 @@
 
 // the settings for this application - no trailing / on the base_url please
 settings = {
-    // used for login
+    // the service layer end-point, change "localhost:8080" to ...
     base_url: 'http://localhost:8080/api',
+    // api version of ws_base
     api_version: 1,
-    // used by web sockets
+    // web sockets platform endpoint for comms
     ws_base: 'http://localhost:8080/ws-api',
-    // the organisation to search
+    // the organisation's id to search
     organisationId: "<organisation>",
     // the knowledge bases inside this organisation to use
     kbList: [{kbId: "<kb>", sid: "<sid>"}],
